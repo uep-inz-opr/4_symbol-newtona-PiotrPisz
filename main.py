17750d890aa5f0819c4ddaae477743f34fb32498
Piotr Piszora <piotr.piszora@gmail.com>
2022-04-23 15:45:31 +0200

    dodany print

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ liczby = input()
 liczby = liczby.split()
 for i in range(len(liczby)):
     liczby[i] = int(liczby[i])
-oblicz_newtona(liczby[0],liczby[1])
+print(oblicz_newtona(liczby[0],liczby[1]))
